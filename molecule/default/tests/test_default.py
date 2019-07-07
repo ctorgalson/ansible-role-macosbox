1c39cd7eb52e83918eaea8043f533c563c55134f
Christopher Torgalson <manager@bedlamhotel.com>
2019-07-07 13:03:36 +0200

    Sets up ssh_key copying etc
    - Adds role to dependencies in meta/main.yml.
    - Adds role to requirements.yml
    - Adds vars to playbook.yml.
    - Adds user creation to prepare.yml.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,7 +11,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.parametrize('user,path,file,mode', [
     ('molecule', '.ssh', '', 0o700,),
     ('molecule', '.ssh', 'id_rsa', 0o600,),
-    ('molecule', '.ssh', 'id_rsa.pub', 0o600,),
+    ('molecule', '.ssh', 'id_rsa.pub', 0o644,),
+    ('molecule', '.ssh', 'authorized_keys', 0o600,),
 ])
 def test_files_and_directories(host, user, path, file, mode):
     f = host.file('/home/{}/{}/{}'.format(user, path, file))
