d1151f8a8b3917a4b34418a927a9ec61728cce3c
Christopher Torgalson <manager@bedlamhotel.com>
2019-07-07 11:49:23 +0200

    Set up first tests to guide role dev

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,13 +2,27 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize('user,path,file,mode', [
+    ('molecule', '.ssh', '', 0o700,),
+    ('molecule', '.ssh', 'id_rsa', 0o600,),
+    ('molecule', '.ssh', 'id_rsa.pub', 0o600,),
+])
+def test_files_and_directories(host, user, path, file, mode):
+    f = host.file('/home/{}/{}/{}'.format(user, path, file))
+
+    if file:
+        is_type = 'is_file'
+    else:
+        is_type = 'is_directory'
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert getattr(f, is_type)
+    assert f.user == user
+    assert f.group == user
+    assert f.mode == mode
