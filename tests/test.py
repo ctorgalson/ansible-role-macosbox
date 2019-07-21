f196c271cb76fcd739502bbb082b544255c76fff
Christopher Torgalson <manager@bedlamhotel.com>
2019-07-21 10:40:30 +0200

    Add more files, mode testing

--- tests/test.py
+++ tests/test.py
@@ -4,9 +4,15 @@ import pytest
 
 import testinfra.utils.ansible_runner
 
-def test_passwd_file(host):
-    d = host.file("/Users/macosbox/.ssh")
+@pytest.mark.parametrize("path,mode", [
+    (".ssh", 0o700),
+    (".ssh/id_rsa", 0o600),
+    (".ssh/id_rsa.pub", 0o644),
+    (".ssh/authorized_keys", 0o600),
+])
+def test_ssh_dir_and_keys(host, mode):
+    d = host.file("/Users/macosbox/{}".format(path))
 
     assert d.user == "macosbox"
     assert d.group == "staff"
-    assert d.mode == 0o700
+    assert d.mode == mode
