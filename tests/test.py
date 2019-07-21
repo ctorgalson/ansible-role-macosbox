0d219c9f912da2a91794c1010b648d447a39bde0
Christopher Torgalson <manager@bedlamhotel.com>
2019-07-21 11:15:00 +0200

    Adds authorized_keys contents check

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,9 @@ import pytest
 
 import testinfra.utils.ansible_runner
 
+
+""" ctorgalson.ssh_keys tests """
+
 @pytest.mark.parametrize("path,mode,file", [
     (".ssh", 0o700, False),
     (".ssh/id_rsa", 0o600, True),
@@ -11,11 +14,19 @@ import testinfra.utils.ansible_runner
     (".ssh/authorized_keys", 0o600, True),
 ])
 def test_ssh_dir_and_keys(host, path, mode, file):
-    t = "is_file" if file else "is_directory"
-    f = host.file("/Users/macosbox/{}".format(path))
+    type = "is_file" if file else "is_directory"
+    file = host.file("/Users/macosbox/{}".format(path))
 
     with host.sudo():
-        assert f.user == "macosbox"
-        assert f.group == "staff"
-        assert f.mode == mode
-        assert getattr(f, t)
+        assert file.user == "macosbox"
+        assert file.group == "staff"
+        assert file.mode == mode
+        assert getattr(file, type)
+
+
+def test_authorized_keys_contents(host):
+    dest = host.file("/Users/macosbox/.ssh/authorized_keys")
+    source = host.file("./files/keys/id_rsa.pub")
+
+    with host.sudo():
+        assert source.content_string in dest.content_string
