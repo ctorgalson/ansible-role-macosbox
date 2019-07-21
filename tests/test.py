5006f44865c25867273a63b1f9ce53acafc6149b
Christopher Torgalson <manager@bedlamhotel.com>
2019-07-21 10:15:51 +0200

    Test w/testinfra?

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+import os
+
+import pytest
+
+import testinfra.utils.ansible_runner
+
+def test_passwd_file(host):
+    d = host.file("/home/macosbox/.ssh")
+
+    assert d.user == "macosbox"
+    assert d.group == "staff"
+    assert d.mode == 0o700
